bad1bbc323c1fa7b4e317e9e0baa6da26d23e3d8
tiago523 <144238162+tiago523@users.noreply.github.com>
2023-11-01 00:39:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
+document.querySelector('.tecla_pom')
+
+
+function tocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector('.tecla_clap')
+
+
+function tocaSomTim() {
+    document.querySelector('#som_tecla_tim').play();
+}
+document.querySelector('.tecla_tim')
+
+
+function tocaSomPuff() {
+    document.querySelector('#som_tecla_puff').play();
+}
+document.querySelector('.tecla_puff')
+
+
+function tocaSomSplash() {
+    document.querySelector('#som_tecla_splash').play();
+}
+document.querySelector('.tecla_splash')
+
+
+function tocaSomToim() {
+    document.querySelector('#som_tecla_toim').play();
+}
+document.querySelector('.tecla_toim')
+
+
+function tocaSomPsh() {
+    document.querySelector('#som_tecla_psh').play();
+}
+document.querySelector('.tecla_psh')
+
+
+function tocaSomTic() {
+    document.querySelector('#som_tecla_tic').play();
+}
+document.querySelector('.tecla_tic')
+
+
+
+function tocaSomTom() {
+    document.querySelector('#som_tecla_tom').play();
+}
+document.querySelector('.tecla_tom')
+
+
+
+
+
+
+
+
