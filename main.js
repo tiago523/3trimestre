bb40850ad0a2e5d6507270ad59c8c0c46056db01
tiago523 <144238162+tiago523@users.noreply.github.com>
2023-11-07 08:52:19 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 function tocaSom(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
-document.querySelectorAll(´.tecla´);
-const listaDeTeclas=document.querySelectorAll(´.tecla´);
+document.querySelectorAll('.tecla');
+const listaDeTeclas=document.querySelectorAll('.tecla');
 let contador = 0;
 //Estrutura de repetição - Enquanto
-while(contador <listaDeTeclas.leigth){
-    listaDeTeclas[0].onclick = tocaSomPom;
+while(contador <listaDeTeclas.length){
+    const instrumento = listaDeTeclas[contador].classlist[1];
+    listaDeTeclas[contador].onclick = function(){
+tocaSom('#som_tecla_splash');
+    };
     contador = contador + 1;
 }
 
