dc6e1a2c6deb8a97c8381321c8e99499ad53c25f
tiago523 <144238162+tiago523@users.noreply.github.com>
2023-11-14 08:52:49 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,45 @@
+//a função que dev ser executada e tocaSom, mas que som? Nesse caso, será o parametro idElementoAudio. Ou seja, o endereço da tecla será procurado no html e resolvido. 
 function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+    document.querySelector(idElementoAudio);
+    //se o elemento pesquisado for igual a "inexistente", o alerta de elemnto não encontrado aparecerá.
 }
-document.querySelectorAll('.tecla');
 const listaDeTeclas=document.querySelectorAll('.tecla');
-let contador = 0;
 //Estrutura de repetição - Enquanto
-while(contador <listaDeTeclas.length){
-    const instrumento = listaDeTeclas[contador].classlist[1];
+for( let contador = 0; contador <listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classlist[1];
+    const idAudio =`#som_$(instrumento)`;
+
+    console.log(idAudio);
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    };
+    tecla.onkeydown = function(evento){
+        if(evento.code ==='Enter' || evento.code ==='Space'){
+            tecla.classlist.add('ativa');
+        }
+    };
+    tecla.onkeyup = function(){
+        tecla.classlist.remove('ativa');
+    };
+    console.log(instrumento);
+console.log(contador);
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+    
     listaDeTeclas[contador].onclick = function(){
 tocaSom('#som_tecla_splash');
     };
